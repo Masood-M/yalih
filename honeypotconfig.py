eb82f1f59b2c3690f8476a1d67a1a3b0110af93f
eternity0022 <mas00d33>
2014-02-18 15:21:48 +1200

    Bug Fixes

--- honeypotconfig.py
+++ honeypotconfig.py
@@ -8,7 +8,9 @@ from os.path import basename
 wdir="/opt/yalih/" #set your working directory - should start and end with "/" without quotations
 useragent="Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)" #Internet explorer identifies itself as Mozilla/4.0 - MSIE 7.0 is Internet Explorer Version and Windows NT 5.1 is Windows XP
 acceptlang="en-us,en;q=0.5"
-referrer="www.google.com"
+referrer="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source="
+# Referrer Google Search "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source="
+
 
 #    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.6) Gecko/2009011913 Firefox/3.0.6",
 #    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.0.6) Gecko/2009011912 Firefox/3.0.6",
