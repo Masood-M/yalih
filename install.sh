6ebec82cac300fa02885542a41b34cffa2bb5534
Masood Mansoori <masood.mansoori@gmail.com>
2018-09-27 14:14:56 +1200

    Fixed

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 
 cd /opt
 
-#apt-get update
+apt-get update
 
 apt-get install -y nano python nano git make gcc g++ autoconf automake libtool python-dev libpcre3-dev flex bison python-setuptools python-lxml
 
@@ -13,14 +13,11 @@ wget https://bootstrap.pypa.io/get-pip.py
 
 python get-pip.py
 
-yes | git clone https://github.com/Masood-M/yalih.git
+#yes | git clone https://github.com/Masood-M/yalih.git
 
 cd yalih
-
 cd jsbeautifier
-
 python setup.py build
-
 python setup.py install
 
 
@@ -31,33 +28,27 @@ cd req
 wget https://github.com/VirusTotal/yara/archive/v3.8.1.tar.gz
 tar -xzvf v3.8.1.tar.gz
 
-cd yara-3.8.1/
-./configure
+cd /opt/yalih/req/yara-3.8.1/
+sudo bash bootstrap.sh
+sudo ./configure
 sudo make
 sudo make install
 
 
 
-cd /opt/yalih/
+#cd /opt/yalih/
+#cd yara-python
+#python setup.py build
+#python setup.py install
 
-cd yara-python
-
-python setup.py build
-
-python setup.py install
-
-echo "/usr/local/lib" >> /etc/ld.so.conf
-
+sudo echo "/usr/local/lib" >> /etc/ld.so.conf
 ldconfig
 
 
-pip install python-magic
-
-pip install psutil
-
-pip install tldextract
-
-pip install mechanize
+sudo pip install python-magic
+sudo pip install psutil
+sudo pip install tldextract
+sudo pip install mechanize
 
 
 #install antiviruses and update them
@@ -66,6 +57,7 @@ sudo wget https://cdn.download.comodo.com/cis/download/installs/linux/cav-linux_
 sudo dpkg -i cav-linux_x86.deb
 
 sudo apt-get install clamav
+sudo bash /opt/COMODO/post_setup.sh
 sudo freshclam
 sudo /opt/COMODO/cavupdater
 ###end of antivirus installations
