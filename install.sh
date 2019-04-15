d62f17c3426beb5f61704c0845acb4518951cfa3
Masood Mansoori <masood.mansoori@ecs.vuw.ac.nz>
2019-04-16 10:58:47 +1200

    minor changes

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-cd /opt
 
 apt-get update
 
@@ -13,22 +12,17 @@ wget https://bootstrap.pypa.io/get-pip.py
 
 python get-pip.py
 
-#yes | git clone https://github.com/Masood-M/yalih.git
 
-cd yalih
 cd jsbeautifier
 python setup.py build
 python setup.py install
 
+cd ../req/
 
-
-cd /opt/yalih/
-
-cd req
-#wget https://github.com/VirusTotal/yara/archive/v3.8.1.tar.gz
+wget https://github.com/VirusTotal/yara/archive/v3.9.0.tar.gz
 tar -xzvf v3.9.0.tar.gz
 
-cd /opt/yalih/req/yara-3.9.0/
+cd yara-3.9.0/
 sudo bash bootstrap.sh
 sudo ./configure
 sudo make
@@ -44,4 +38,3 @@ sudo pip install python-magic
 sudo pip install psutil
 sudo pip install tldextract
 sudo pip install mechanize
-
